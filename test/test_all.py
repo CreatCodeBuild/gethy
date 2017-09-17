d8163ae25c9e75820736ac0c877ecf419088d5c8
Albert Wang <wangxuanzhealbert@gmail.com>
2017-09-17 00:31:00 -0700

    完善测试
    还有 flow control 的部分没有测试

--- test/test_all.py
+++ test/test_all.py
@@ -37,19 +37,57 @@ def test_receive():
 	assert event.stream.stream_ended is True
 
 
-# def test_send():
-#
-# 	headers = [
-# 		(':status', '200'),
-# 		('content-length', '0')
-# 	]
-#
-# 	stream = Stream(1, headers)
-# 	stream.stream_ended = True
-# 	stream.buffered_data = None
-# 	stream.data = b''
-#
-# 	e = p.send(stream)
+def test_send_headers_only():
+
+	headers = [
+		(':status', '200'),
+		('content-length', '0')
+	]
+
+	stream = Stream(1, headers)
+	stream.stream_ended = True
+	stream.buffered_data = None
+	stream.data = b'0'
+
+	e = p.send(stream)
+	print(e)
+	for ev in e:
+		print(ev.data)
+	# assert len(e) == 1
+	# assert isinstance(e[0], RequestEvent)
+
+
+def test_send_headers_and_data():
+
+	headers = [
+		(':method', 'POST'),
+		(':path',   '/'),
+		(':scheme', 'https'),
+		(':authority', 'example.com'),
+	]
+
+	frame_factory = FrameFactory()
+
+	# p.receive(frame_factory.preamble())
+
+	f1 = frame_factory.build_headers_frame(headers, stream_id=3)
+	f2 = frame_factory.build_data_frame(b'1', stream_id=3, flags=['END_STREAM'],)
+	data = b''.join(map(lambda f: f.serialize(), [f1, f2]))
+	e = p.receive(data)
+
+
+def test_f():
+	headers = [
+		(':status', '200'),
+		('content-length', '1'),
+	]
+
+	stream = Stream(3, headers)
+	stream.stream_ended = True
+	stream.buffered_data = None
+	stream.data = b'1'
+
+	e = p.send(stream)
 
 	# assert len(e) == 1
 	# assert isinstance(e[0], RequestEvent)
